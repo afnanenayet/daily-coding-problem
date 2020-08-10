baeee9835e23f7d3e08ab0a40035812115d22294
Afnan Enayet <afnan@afnan.io>
2020-08-10 07:59:56 -0400

    Add clang-format config, format files
    Use 4-column indents

--- cpp/include/pch.hpp
+++ cpp/include/pch.hpp
@@ -2,9 +2,9 @@
 //
 // Add all common includes here
 
+#include <catch2/catch.hpp>
 #include <iostream>
 #include <queue>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
-#include <catch2/catch.hpp>
