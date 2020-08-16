a6e0e81db30b4b96f7c7fc2605bc51084fb3b9ff
Afnan Enayet <afnan@afnan.io>
2020-08-16 16:10:24 -0400

    Complete problem

---
+++ cpp/src/582.cpp
@@ -0,0 +1,68 @@
+/*
+Let X be a set of n intervals on the real line. We say that a set of points P
+"stabs" X if every interval in X contains at least one point in P. Compute the
+smallest set of points that stabs X.
+
+For example, given the intervals [(1, 4), (4, 5), (7, 9), (9, 12)], you should
+return [4, 9].
+ */
+
+#include "pch.hpp"
+
+namespace p582 {
+// A struct representing some interval
+class Interval {
+  public:
+    int start;
+    int end;
+
+    bool operator==(const Interval &other) const {
+        return this->start == other.start && this->end == other.end;
+    }
+};
+
+std::ostream &operator<<(std::ostream &os, Interval const &value) {
+    os << "{" << value.start << ", " << value.end << "}";
+    return os;
+}
+
+// The sorting comparison function to use when sorting intervals
+bool compare_interval(const Interval &a, const Interval &b) {
+    return a.end < b.end;
+}
+
+// Compute the minimal interval that is "stabbed" by all of the input intervals
+//
+// This is O(n) space and O(nlogn) time. You could reduce memory usage by
+// sorting in-place, but I generally assume that the inputs are immutable.
+Interval minimal_interval(const std::vector<Interval> &input_list) {
+    if (input_list.empty()) {
+        throw std::invalid_argument("Input list must not be empty");
+    }
+    auto intervals = input_list;
+
+    // First, we sort the intervals by their endpoints
+    std::sort(intervals.begin(), intervals.end(), compare_interval);
+    auto stab_interval =
+        Interval{intervals.begin()->end, intervals.begin()->end};
+
+    // We extend the end of the interval by the start value of the next interval
+    // if the next interval isn't already stabbed by our stab interval. Since
+    // we've sorted by endpoint, we know that the endpoint of the stab interval
+    // is less than or equal to the endpoint of the current interval, and the
+    // minimal interval we need to stab the current interval is to extend the
+    // stab interval minimally so that it covers the interval. If the current
+    // stab interval has an endpoint that is greater than the current interval,
+    // then the current interval has already been stabbed.
+    for (const auto &interval : intervals) {
+        stab_interval.end = std::max(stab_interval.end, interval.start);
+    }
+    return stab_interval;
+}
+
+TEST_CASE("misc examples", "[582]") {
+    REQUIRE(minimal_interval({{1, 4}, {4, 5}, {7, 9}, {9, 12}}) ==
+            Interval{4, 9});
+    REQUIRE(minimal_interval({{0, 4}, {1, 2}, {-3, 7}}) == Interval{2, 2});
+}
+}; // namespace p582
