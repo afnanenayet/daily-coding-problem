2cf34e0926544bf1f29ac8590c2a265f831572bd
Afnan Enayet <afnan@afnan.io>
2020-09-21 23:45:06 -0500

    Speed up IO
    Update the main function and disable C stdio compatibility to get a
    boost in IO speeds

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,5 +1,12 @@
-#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this
-                          // in one cpp file
+#define CATCH_CONFIG_RUNNER
+
 #include <catch2/catch.hpp>
+
+int main(int argc, char *argv[]) {
+    // We don't care about C STDIO compability, so turning this off will speed
+    // up I/O significantly
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
 
 TEST_CASE("Sanity check", "[main]") { REQUIRE(true); }
